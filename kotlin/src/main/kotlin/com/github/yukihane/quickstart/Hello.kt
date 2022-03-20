9cdf1eb76be46f90216cd94228cf90f1a18be5bc
yukihane <yukihane.feather@gmail.com>
2022-03-20 23:42:35 +0900

    initial sources

---
+++ kotlin/src/main/kotlin/com/github/yukihane/quickstart/Hello.kt
@@ -0,0 +1,6 @@
+package com.github.yukihane.quickstart
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ kotlin/src/test/kotlin/com/github/yukihane/quickstart/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.github.yukihane.quickstart
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
